144f5448bc5bb15ffac408faca108eb7bc0eec97
Michael A. Fons <mfons@squaretwofinancial.com>
2018-01-24 07:41:25 -0700

    added a wee bit of detail to sync function code

--- src/background-sync-functions.js
+++ src/background-sync-functions.js
@@ -1,1 +1,14 @@
 console.log("background-sync:  Hello World!");
+self.addEventListener('sync', function (event) {
+    if (event.tag == 'eatSomething') {
+        event.waitUntil(eatSomething());
+    }
+});
+
+function  eatSomething() {
+    return new Promise((resolve, reject) => {
+        // TODO read from redux queue of things to eat,
+        // write it to firebase somehow.
+        resolve();
+    });
+}
