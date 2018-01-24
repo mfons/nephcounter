1267cb2e1269a6e0a06fda2331dd4004b3e1c973
Michael A. Fons <mfons@squaretwofinancial.com>
2018-01-24 07:56:16 -0700

    put a console log into the sync so I could play with the sync event generator in dev tools service worker pane.

--- src/background-sync-functions.js
+++ src/background-sync-functions.js
@@ -9,6 +9,7 @@ function  eatSomething() {
     return new Promise((resolve, reject) => {
         // TODO read from redux queue of things to eat,
         // write it to firebase somehow.
+        console.info("sync: eatSomething is happening.");
         resolve();
     });
 }
