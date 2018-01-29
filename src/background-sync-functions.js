c7e7143b1a1ebee5c56bac6a5639c2ef1fb3530a
Michael A. Fons <mfons@blm.gov>
2018-01-29 15:50:42 -0700

    re-re-did the plan how to implement and put in comments to guide coding.

--- src/background-sync-functions.js
+++ src/background-sync-functions.js
@@ -21,7 +21,12 @@ function eatSomething(addedId) {
     return new Promise((resolve, reject) => {
         const {port} = syncStore[addedId] || {};
         delete syncStore[addedId];
+        // send a message to try to talk to firebase
         port.postMessage(addedId);
+        // TODO instead of talking back and forth we should just 
+        // talk through the indexedDB record...so the test here
+        // would be to reject if the indexeddb record exists, and 
+        // resolve if it is gone.
         console.info("sw sync event: we are online (for the moment); please send the following eaten item to firebase storage:", addedId);
         resolve();
     });
