8dcaaa656f03ab40ce534eeb65f72cd9e38ed372
Michael A. Fons <mfons@blm.gov>
2018-01-30 14:46:00 -0700

    continuing to work offline saves

--- src/background-sync-functions.js
+++ src/background-sync-functions.js
@@ -1,14 +1,12 @@
-const syncStore = [];
+//const syncStore = [];
 
 self.addEventListener('message', event => {
-//if (isObject(event.data)) {
-        if (event.data.type === 'somethingWasEaten') {
-            //     const id = event.data.id || uuid()
-            // pass the port into the memory store
-            syncStore[event.data.addedId] = Object.assign({ port: event.ports[0] }, event.data);
-            self.registration.sync.register(event.data.addedId);
-        }
-//    }
+    if (event.data.type === 'somethingWasEaten') {
+        //     const id = event.data.id || uuid()
+        // pass the port into the memory store
+        syncStore[event.data.addedId] = Object.assign({ port: event.ports[0] }, event.data);
+        self.registration.sync.register(event.data.addedId);
+    }
     console.info("message sent to service worker: ", event.data);
 });
 
@@ -19,15 +17,60 @@ self.addEventListener('sync', function (event) {
 
 function eatSomething(addedId) {
     return new Promise((resolve, reject) => {
-        const {port} = syncStore[addedId] || {};
-        delete syncStore[addedId];
-        // send a message to try to talk to firebase
-        port.postMessage(addedId);
-        // TODO instead of talking back and forth we should just 
-        // talk through the indexedDB record...so the test here
+        // const { port } = syncStore[addedId] || {};
+        // delete syncStore[addedId];
+        // Don't think we need to send a message: 
+        // port.postMessage(addedId);
+        // Instead of messaging back and forth we just 
+        // "talk" through the indexedDB record...so the test here
         // would be to reject if the indexeddb record exists, and 
         // resolve if it is gone.
-        console.info("sw sync event: we are online (for the moment); please send the following eaten item to firebase storage:", addedId);
-        resolve();
+        areThereRecordsInTheQueue()
+            .then(() => { resolve();})
+            .catch(() => { reject();});
+        console.info("sw sync event: ", addedId);
+    });
+}
+
+function areThereRecordsInTheQueue() {
+    return new Promise((resolve, reject) => {
+        var db;
+        var request = window.indexedDB.open("NephcounterOffLineDB");
+        request.onerror = function (event) {
+            // Do something with request.errorCode!
+            console.log("NephcounterOffLineDB creation failed!", event)
+            reject("open of database failed with error!");
+        }; // end of onerror
+        request.onsuccess = function (event) {
+            console.log("NephcounterOffLineDB open succeeded!");
+            db = event.target.result;
+            if (!db.objectStoreNames.contains('eatenItemsQueue')) {
+                // TODO should this be a "resolve" or a "reject"?
+                reject("eatenItemsQueue object store has not been created yet to save to.");
+                console.warn("eatenItemsQueue object store has not been created yet to save to.", db.objectStoreNames);
+                db.close();
+                return;
+            }
+            var objectStore = db.transaction(["eatenItemsQueue"], "readonly").objectStore("eatenItemsQueue");
+            objectStore.openCursor().onsuccess = function (event) {
+                var cursor = event.target.result;
+                if (cursor) {
+                    //alert("Name for SSN " + cursor.key + " is " + cursor.value.name);
+                    //cursor.continue();
+                    reject(); // tell sync to try again later
+                }
+                else {
+                    //alert("No more entries!");
+                    resolve();
+                }
+                db.close();
+            };
+        }; // end of onsuccess
+
+        // This is done only if there is no database and one needs to be created before we read from it.
+        request.onupgradeneeded = function (event) {
+            console.log("an upgrade is needed...");
+            var db = event.target.result;
+        }; // end of onupgradeneeded
     });
 }
