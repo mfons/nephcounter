f3ff273d3337438653918e790ca33da06f47360d
Michael A. Fons <mfons@blm.gov>
2018-02-09 11:41:28 -0700

    used async/await (for the first time ever :-)) to see if there was something different about it in airplane mode.  The behavior I am seeing is that the events are held up from arriving at the event handler while we are in airplane mode.  Or the registration does not take place if it registers itself in airplane mode, or it takes place, but the registration does not generate a sync event until it goes out of airplane mode.  Does the sw state change when in airplane mode?

--- src/background-sync-functions.js
+++ src/background-sync-functions.js
@@ -1,17 +1,17 @@
 const syncStore = [];
 
-self.addEventListener('message', event => {
+self.addEventListener('message', async (event) => {
     if (event.data.type === 'somethingWasEaten') {
         //     const id = event.data.id || uuid()
         // pass the port into the memory store
         syncStore[event.data.addedId] = Object.assign({ port: event.ports[0] }, event.data);
-        /*await*/ self.registration.sync.register(event.data.addedId)
-            .then(() => {
-                console.log('Sync registered');
-            })
-            .catch((err) => {
-                console.warn('Error with sync registration: ', err);
-            });
+        await self.registration.sync.register(event.data.addedId);
+            // .then(() => {
+            //     console.log('Sync registered');
+            // })
+            // .catch((err) => {
+            //     console.warn('Error with sync registration: ', err);
+            // });
     }
     console.info("message sent to service worker: ", event.data);
 });
